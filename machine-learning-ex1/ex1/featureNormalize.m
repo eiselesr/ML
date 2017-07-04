f644dfd6a80cccec42be6189a896581c8a9931f6
Scott Eisele <eiselesr@gmail.com>
2017-07-04 17:19:46 -0400

    week 2 linear regression

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -26,8 +26,13 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-
-
+mu = mean(X);
+sigma = std(X);
+sigma( sigma ==0) =1;
+m = size(X, 1);
+mu_matrix =  ones(m, 1) * mu;
+sigma_matrix = ones(m, 1) * sigma;
+X_norm = (X - mu_matrix)./sigma_matrix;
 
 
 
